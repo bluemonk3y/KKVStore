b9eaf5da567c6d96b25eca4c6a11809f86ba39de
logscape <neil.avery@logscape.com>
2017-07-20 11:18:26 +0100

    mavening the project

---
+++ src/main/java/com/blu3monk3y/kkvstore/App.java
@@ -0,0 +1,13 @@
+package com.blu3monk3y.kkvstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
